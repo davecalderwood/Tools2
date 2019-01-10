5cd331be26775767370b6c1d2970891bff6f2f74
Dave Calderwood <davidhcalderwood@gmail.com>
2019-01-10 10:44:04 -0700

    added calendar/just needs css

--- src/pages/Calendar.js
+++ src/pages/Calendar.js
@@ -1,11 +1,101 @@
 import React, { Component } from "react";
+// import Calendar from 'react-calendar';
+import Week from '../components/Week';
+import DayNames from '../components/DayNames';
+import moment from 'moment';
+
 
 export default class Calendar extends Component {
-    render(){
-    return (
-        <div className="component">
-            <div className="calendar">Calendar</div>
-        </div>
-    )
+    constructor(props) {
+      super(props);
+      
+      this.state = {
+        month: moment(),
+        selected: moment().startOf('day')
+      };
+      
+      this.previous = this.previous.bind(this);
+      this.next = this.next.bind(this);
     }
-}
+    
+    previous() {
+      const {
+        month,
+      } = this.state;
+  
+      this.setState({
+        month: month.subtract(1, 'month'),
+      });
+    }
+  
+    next() {
+      const {
+        month,
+      } = this.state;
+  
+      this.setState({
+        month: month.add(1,'month'),
+      });
+    }
+    
+    select(day) {
+      this.setState({
+        selected: day.date,
+        month: day.date.clone(),
+      });
+    }
+  
+    renderWeeks() {
+      let weeks = [];
+      let done = false;
+      let date = this.state.month.clone().startOf("month").add("w" -1).day("Sunday");
+      let count = 0;
+      let monthIndex = date.month();
+  
+      const {
+        selected,
+        month,
+      } = this.state;
+  
+      while (!done) {
+        weeks.push(
+          <Week key={date} 
+            date={date.clone()} 
+            month={month} 
+            select={(day)=>this.select(day)} 
+            selected={selected} />
+        );
+  
+        date.add(1, "w");
+        
+        done = count++ > 2 && monthIndex !== date.month();
+        monthIndex = date.month();
+      }
+  
+      return weeks;
+    };
+  
+    renderMonthLabel() {
+      const {
+        month,
+      } = this.state;
+  
+      return <span className="month-label">{month.format("MMMM YYYY")}</span>;
+    }
+  
+    render() {
+      return (
+        <section className="calendar">
+          <header className="header">
+            <div className="month-display row">
+              <i className="arrow fa fa-angle-left" onClick={this.previous}/>
+              {this.renderMonthLabel()}
+              <i className="arrow fa fa-angle-right" onClick={this.next}/>
+            </div>
+            <DayNames />
+          </header>
+          {this.renderWeeks()}
+        </section>
+      );
+    }
+  }
