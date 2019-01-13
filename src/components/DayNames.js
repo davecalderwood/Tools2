0dd29c64203d8e1fd626bef257f870ecc3acb97c
Dave Calderwood <davidhcalderwood@gmail.com>
2019-01-12 18:25:02 -0700

    Fixed sizing

--- src/components/DayNames.js
+++ src/components/DayNames.js
@@ -4,13 +4,13 @@ export default class DayNames extends Component {
     render() {
         return (
           <div className="row day-names">
-            <span className="day">Sun</span>
-            <span className="day">Mon</span>
-            <span className="day">Tue</span>
-            <span className="day">Wed</span>
-            <span className="day">Thu</span>
-            <span className="day">Fri</span>
-            <span className="day">Sat</span>
+            <span className="day">Sunday</span>
+            <span className="day">Monday</span>
+            <span className="day">Tuesday</span>
+            <span className="day">Wednesday</span>
+            <span className="day">Thursday</span>
+            <span className="day">Friday</span>
+            <span className="day">Saturday</span>
           </div>
         );
     }
