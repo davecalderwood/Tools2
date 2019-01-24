8debb789b1b09f429c38062b4d575c1aad83d85b
Dave Calderwood <davidhcalderwood@gmail.com>
2019-01-24 12:32:03 -0700

    Ball animaion

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,9 +3,20 @@ import React, { Component } from "react";
 export default class Home extends Component {
     render(){
     return (
-        <div className="component">
-            <div className="home">Home</div>
+    <div className="component">
+        <div class="wrap red">
+            <div class="shadow"></div>
+            <div class="ball"></div>
+            </div>
+            <div class="wrap green">
+            <div class="shadow"></div>
+            <div class="ball"></div>
+            </div>
+            <div class="wrap blue">
+            <div class="shadow"></div>
+            <div class="ball"></div>
         </div>
+    </div>
     )
     }
 }
