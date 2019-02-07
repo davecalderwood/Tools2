0ec1335bbeab22ebbb698ca2f8e5b99c88c5f3c1
Dave Calderwood <davidhcalderwood@gmail.com>
2019-02-07 14:56:58 -0700

    Adding buttons on about pages

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,6 +6,31 @@ export default class About extends Component {
         <div className="component">
             <div className="about">I made this app to continue my work on React and to keep my skills honed while I look for a job in the field. </div>
             <img className="waterski" src="../Waterski.jpg" alt="Waterski"></img>
+            <a 
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://github.com/davecalderwood" 
+            className="action-button shadow animate blue">
+            GitHub</a>
+
+            <a 
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://www.linkedin.com/in/david-calderwood/" 
+            className="action-button shadow animate red">
+            Linkedin</a>
+
+            <a 
+            href="mailto:davidhcalderwood@gmail.com" 
+            className="action-button shadow animate green">
+            Email</a>
+            <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://www.facebook.com/david.calderwood35" 
+            className="action-button shadow animate yellow">
+            Facebook</a>
+
         </div>
     )
     }
