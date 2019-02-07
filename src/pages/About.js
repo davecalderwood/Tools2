1c5c7e098081a07d1ac146be96d91675c4b48763
Dave Calderwood <davidhcalderwood@gmail.com>
2019-02-07 14:40:49 -0700

    Added information on about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,7 +4,7 @@ export default class About extends Component {
     render(){
     return (
         <div className="component">
-            <div className="about">About</div>
+            <div className="about">I made this app to continue my work on React and to keep my skills honed while I look for a job in the field. </div>
         </div>
     )
     }
